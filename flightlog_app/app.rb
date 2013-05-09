4bd1b4d91a78d1c2fdfed7c3d4a0bd27c7b98e54
Marshel Helsper <marsol0x@gmail.com>
2013-05-08 22:51:25 -0400

    Started with some HAML templating
    + Deleted unnecessary stylesheets and javascript files

--- flightlog_app/app.rb
+++ flightlog_app/app.rb
@@ -1,4 +1,5 @@
 require 'sinatra'
+require 'haml'
 
 # This is so Sinatra's built-in web server will listen outside
 # of localhost. So to use the server through vagrant, we need this.
@@ -6,3 +7,7 @@ set :bind, '0.0.0.0'
 
 # Static location
 set :public_folder, File.dirname(__FILE__) + '/static'
+
+get '/' do
+  haml :index, :format => :html5, :locals => { :message => "Hello!" }
+end
